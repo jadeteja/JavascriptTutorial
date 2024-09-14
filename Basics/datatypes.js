e7ae44b819dc14a74af047837363344edb00be5c
Teja Jade <73740996+jadeteja@users.noreply.github.com>
2024-09-14 02:51:26 +0000

    conversion of datatypes

---
+++ Basics/datatypes.js
@@ -0,0 +1,17 @@
+"use strict"; // Treat all js code as a newer version 
+
+// alert("Hello"); // we are using nodejs not browser
+console.log("Hello World");
+
+//number => 2 to power 3
+//bigint
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined =>
+//symbol  => unique
+
+
+//object
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
