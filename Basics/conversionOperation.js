7a8951ac75bbb5422b739e57573bc3088d06ca69
Teja Jade <73740996+jadeteja@users.noreply.github.com>
2024-09-14 03:55:10 +0000

    conversion operation

--- Basics/conversionOperation.js
+++ Basics/conversionOperation.js
@@ -1,17 +1,53 @@
-let score = null;
-console.log(typeof score);
+// let score = null;
+// console.log(typeof score);
 
-let valueInNumber = Number(score);
-console.log(valueInNumber);
+// let valueInNumber = Number(score);
+// console.log(valueInNumber);
 
 
-// "33" => 33
-// "33abc" => NaN (not a number)
-// true => 1; false => 0
+// // "33" => 33
+// // "33abc" => NaN (not a number)
+// // true => 1; false => 0
 
-let isLoggedIn = 1
-let booleanIsLoggedIn = Boolean(isLoggedIn);
-console.log(booleanIsLoggedIn);
+// let isLoggedIn = 1
+// let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
 
-// 1 => true; 0 => false
-// 
+// // 1 => true; 0 => false
+// // 
+
+// let num = 45;
+
+// let stringNum = String(num);
+
+// console.log(stringNum);
+// console.log(typeof(stringNum));
+
+// let value = 3;
+// let negValue = -value;
+
+// console.log(negValue);
+
+
+// let str1 = "hello"
+// let str2 = " Teja"
+
+// let str3 = str1 + str2;
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log("2" + 1);
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// let num1, num2, num3;
+// prefix: values are first incremented later assigned
+// postfix: values are assigned later incremented
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+// The reason is that an equality check == and comparisons > < >= <= work differently.comparisons convert null to number, treating it as 0
+
+// === (It checks for value and datatype )
